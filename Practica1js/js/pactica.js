dad3ec2b61ec3dfc665c33cb5ce35bfcf9246d7e
enumerates <kevin.gonzalez2027@frech.superate.org>
2026-09-10 09:51:14 -0600

    nooooo teacher me esta echando

---
+++ Practica1js/js/pactica.js
@@ -0,0 +1,17 @@
+document.write("hola mundo");
+alert("hola mundo");
+console.log("hola mundo");
+
+let nombre = "Ana";
+let edad = 25;
+let estudiantes = true;
+let ciudad;
+let mascota=null;
+
+console.log(typeof nombre);
+console.log(typeof edad);
+console.log(typeof estudiantes);
+console.log(typeof ciudad);
+console.log(typeof mascota);
+
+
